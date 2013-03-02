b37a0b496bb512b1aa76587b2e0c6de4ab11eaa1
Uli <m@il.wolf-u.li>
2013-03-03 00:24:28 +0100

    Added texinfo, libgpg-error. Updated libassuan, uwchmod and definitions/libgpg-error/DESCRdefinitions/libgpg-error/DESCR

--- 0.7/ffpcompiler/definitions/uwchmod/ffp/var/opt/uwchmod/uwchmod.sh
+++ 0.7/ffpcompiler/definitions/uwchmod/ffp/var/opt/uwchmod/uwchmod.sh
@@ -13,15 +13,18 @@ for V_CHMODVARTEST in $V_ARGUMENTS; do
                 # Could be a problem, lets check further
 		# Get the path
 		V_ARGUMENTS_PATH=$(echo $V_ARGUMENTS|awk '{for(i=1;i<=NF;i++)if($i ~/\//) print $i}')
-		# Remove last slash
-		V_ARGUMENTS_PATH=${V_ARGUMENTS_PATH%/*}
-		# Determine possible paths
-		for V_DIRECTORY in $(find $V_STARTDIR -maxdepth 2 -type d -name "USB*" -o -name "HD*"); do
-			if [[ $V_DIRECTORY = $V_ARGUMENTS_PATH ]]; then
-				V_PROBLEMATICSTATEMENT=1
-				break
-			fi
-		done
+		# If this is empty, this is not from the firmware, can be skipped
+		if [[ "${V_ARGUMENTS_PATH}test" != "test" ]]; then
+			# Remove last slash
+			V_ARGUMENTS_PATH=${V_ARGUMENTS_PATH%/*}
+			# Determine possible paths
+			for V_DIRECTORY in $(find $V_STARTDIR -maxdepth 2 -type d -name "USB*" -o -name "HD*"); do
+				if [[ $V_DIRECTORY = $V_ARGUMENTS_PATH ]]; then
+					V_PROBLEMATICSTATEMENT=1
+					break
+				fi
+			done
+		fi
         fi
 done
 
